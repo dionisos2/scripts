924054a0597944bd18f5bcf5522e4cb4f3abfaf0
dionisos2 <denis.baudouin@gmail.com>
2022-06-17 14:42:00 +0200

    "Add create_julia_project.jl"

---
+++ create_julia_project.jl
@@ -0,0 +1,9 @@
+#!/usr/bin/julia
+
+if length(ARGS) == 1
+  println("Create $(ARGS[1]) project")
+  create_template()
+  my_template(ARGS[1])
+else
+  println("Use create_julia_project.jl ProjectName")
+end
